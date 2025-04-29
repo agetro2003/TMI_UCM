a5ed033e982d18b9910454b1b97cb3c0af9996c9
agetro2003 <jesusdaniolob@gmail.com>
2025-04-29 18:53:07 +0200

    refactor: change names on env variables

--- api/axios.js
+++ api/axios.js
@@ -1,11 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5000';
+const BASE_URL = process.env.API_URL || "http://localhost:5000";
 
 export const classifier = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    });
-
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
